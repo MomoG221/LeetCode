8dabee2aa984a5addc544376957460ccc6d4f386
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-12-18 18:33:41 -0500

    1431 done

---
+++ 1431. Kids With the Greatest Number of Candies/Solution.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        int max  = 0 ;
+
+        for (int i = 0 ; i < candies.length ; i++){
+            
+            if (candies[i] > max){
+                max = candies[i];
+            }
+
+        }
+
+        List<Boolean> result = new ArrayList<>();
+
+        for (int i = 0 ; i < candies.length ; i++){
+            
+            if (candies[i] + extraCandies >= max){
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+
+        return result;
+        
+    }
+    
+    // Main method for testing
+    public static void main(String[] args) {
+    
+    int [] candies = {2,3,5,1,3};
+    int extraCandies = 3;
+
+    Solution solution = new Solution();
+    List<Boolean> result = solution.kidsWithCandies(candies, extraCandies);
+    System.out.println("Test Case 1: " + result); // Expected: [true,true,true,false,true]
+
+
+        
+    }
+}
